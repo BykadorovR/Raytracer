835e75167088ddf394c7dfd7fdb75cdf3778fcf5
BykadorovR <bykadorov.r@gmail.com>
2024-02-18 09:36:18 +0100

    feat: particles folder

--- samples/particleSystem/download.py
+++ samples/particleSystem/download.py
@@ -1,6 +1,4 @@
 import gdown
 
-shape = "https://drive.google.com/drive/folders/1FOrHziuHYXzvVFmpuMu11r7oLQreagau?usp=sharing"
-sprite = "https://drive.google.com/drive/folders/1DDqN1h_3FEjIYjyaP7ou1lvynHzDp2Wm?usp=sharing"
+particles = https://drive.google.com/drive/folders/1V69LVLnKFMAJCzBJ8D5PoVrZTvkzW1se?usp=sharing
 gdown.download_folder(url=shape, output="assets/", quiet=False, use_cookies=False)
-gdown.download_folder(url=sprite, output="assets/", quiet=False, use_cookies=False)
